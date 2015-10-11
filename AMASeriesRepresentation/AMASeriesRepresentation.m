630cdbf67c89c7d9e4baae4c7008264c72f4e142
Gary Anderson <asymptoticallystable@gmail.com>
2015-10-11 17:01:13 -0400

    better path gen

--- AMASeriesRepresentation/AMASeriesRepresentation.m
+++ AMASeriesRepresentation/AMASeriesRepresentation.m
@@ -357,19 +357,27 @@ With[{theZs=Join @@ Reverse[Drop[Reverse/@genZVars[numTerms,Length[psiZ[[1]]]],1
 
 
 computeXtPath[linMod:{BB_?MatrixQ,phi_?MatrixQ,FF_?MatrixQ,psiEps_?MatrixQ,psiC_?MatrixQ,psiZ_?MatrixQ},
-	numZTerms_Integer,postZTerms_Integer]:=
-	With[{initNonZPart=computNonFPart[linMod],allZs=genZVars[numZTerms-1,Length[psiZ[[1]]],0]},
-		With[{allFPows=ArrayFlatten[{Table[computeFPartK[FF,phi,psiZ,ii],{ii,numZTerms-1}]}]},
-		FoldList[doStep[linMod,initNonZPart,#]&,allZs]]]
+	numZTerms_Integer,addZeroZTerms_Integer]:=
+		With[{zeroEps=0*Transpose[{Private`genEpsVars[Length[psiEps[[1]]]]}]},
+			With[{trips=
+				 NestList[{doStep[linMod,#[[1]],#[[2]],#[[3]]],zeroEps,Drop[#[[3]],1]}&,
+				 	{Transpose[{Private`genXtm1Vars[Length[BB]]}],
+				 Transpose[{Private`genEpsVars[Length[psiEps[[1]]]]}], 
+				 Reverse /@ genZVars[numZTerms-1, Length[psiZ[[1]]],0]},numZTerms]},
+				 With[{pathNow=Join @@ First/@ trips,theEnd=BB .trips[[-1,1]]},
+				 	With[{extraEnd=Join @@ NestList[BB.#&,theEnd,addZeroZTerms]},
+				 	Join[pathNow,extraEnd]
+				 ]]]]
 		
 doStep[linMod:{BB_?MatrixQ,phi_?MatrixQ,FF_?MatrixQ,psiEps_?MatrixQ,psiC_?MatrixQ,psiZ_?MatrixQ},
 	xxNow_?MatrixQ,epsNow_?MatrixQ,zsNow:{_?MatrixQ...}]:=		
-	BB . xxNow + phi .psiEps. epsNow+sumZs[FF,psiZ,zsNow]
+	BB . xxNow + phi .psiEps. epsNow+sumZs[FF,phi,psiZ,zsNow]
 	
-sumZs[FF_?MatrixQ,psiZ_?MatrixQ,zsNow:{_?MatrixQ...}]:=
-With[{allFPows=ArrayFlatten[{Table[computeFPartK[FF,phi,psiZ,ii],{ii,0,Length[zsNow]}]}]},
-	allZPows.theZs]
-
+sumZs[FF_?MatrixQ,phi_?MatrixQ,psiZ_?MatrixQ,zsNow:{_?MatrixQ..}]:=
+With[{allFPows=ArrayFlatten[{Table[computeFPartK[FF,phi,psiZ,ii],{ii,0,Length[zsNow]-1}]}]},
+	allFPows.Transpose[{Flatten[zsNow]}]]
+	
+sumZs[FF_?MatrixQ,phi_?MatrixQ,psiZ_?MatrixQ,{}]:=0
 
 
 computeNextXt[linMod:{BB_?MatrixQ,phi_?MatrixQ,FF_?MatrixQ,psiEps_?MatrixQ,psiC_?MatrixQ,psiZ_?MatrixQ},numTerms_Integer]:=(*
